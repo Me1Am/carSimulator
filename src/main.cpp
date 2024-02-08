d6486474d49795165847db2f3a8b58483d3affb4
Me1Am <jtkowalew@gmail.com>
2024-02-07 22:11:39 -0500

    Added missing Car() parameters

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
 	std::cout << "start" << std::endl;
 
-	float wheelArgs[4][3] = {{0.5, 1.0, true}, {0.5, 1.0, true},
-							 {0.5, 1.0, false}, {0.5, 1.0, false}};
+	float wheelArgs[4][4] = {{25, 0.5, 1.0, true}, {25, 0.5, 1.0, true},
+							 {25, 0.5, 1.0, false}, {25, 0.5, 1.0, false}};
 	float constCar[] = {9075, 0.4, 1.5, 7.0, 5.5, 0.4257, 12.8, 4};
 	float constEngine[] = {2100, 1200, 2730, 2, false, 0.80, 2.30};
 
